ae170859b5fff904e6a32a53988d6002afafeb58
Norayr Sargsyan <Norayr.Sargsyan@sflpro.local>
2019-09-10 16:58:49 +0400

    new line at the end of file

--- healthcheck1.sh
+++ healthcheck1.sh
@@ -1,12 +1,12 @@
-#!/bin/sh
+#!/bin/bash
 # Environment Variables
 # HUB_HOST
 # BROWSER
 # MODULE
 echo "Checking if hub is ready - $HUB_HOST"
-while [[ "$( curl -s http://$HUB_HOST:4444/wd/hub/status | jq -r .value.ready )" != "true" ]];
+while [[ "$( curl -s http://$HUB_HOST:4444/wd/hub/status | jq -r .value.ready )" != "true" ]]
 do
-    sleep 2
+	sleep 2
 done
 java -cp selenium-docker.jar:selenium-docker-tests.jar:libs/* \
     -DHUB_HOST=$HUB_HOST \
