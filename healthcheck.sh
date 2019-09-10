3eb0686926eb038cc5e5b23317475423db48abb6
Norayr Sargsyan <Norayr.Sargsyan@sflpro.local>
2019-09-10 14:57:37 +0400

    ddd

--- healthcheck.sh
+++ healthcheck.sh
@@ -4,9 +4,9 @@
 # BROWSER
 # MODULE
 echo "Checking if hub is ready - $HUB_HOST"
-while [true]
+while [[ "$( curl -s http://$HUB_HOST:4444/wd/hub/status | jq -r .value.ready )" != "true" ]]
 do
-	echo "myau"
+	sleep 2
 done
 java -cp selenium-docker.jar:selenium-docker-tests.jar:libs/* \
     -DHUB_HOST=$HUB_HOST \
