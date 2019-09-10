00aa48fea9024a5aa2e1f0e664e070bc2bb8a5c4
Norayr Sargsyan <Norayr.Sargsyan@sflpro.local>
2019-09-10 14:26:35 +0400

    ds

--- healthcheck.sh
+++ healthcheck.sh
@@ -3,7 +3,6 @@
 # HUB_HOST
 # BROWSER
 # MODULE
-
 echo "Checking if hub is ready - $HUB_HOST"
 while [ "$( curl -s http://$HUB_HOST:4444/wd/hub/status | jq -r .value.ready )" != "true" ]
 do
@@ -13,4 +12,3 @@ java -cp selenium-docker.jar:selenium-docker-tests.jar:libs/* \
     -DHUB_HOST=$HUB_HOST \
     -DBROWSER=$BROWSER \
     org.testng.TestNG $MODULE
-
